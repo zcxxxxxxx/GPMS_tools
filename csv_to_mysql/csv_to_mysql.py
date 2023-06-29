894ce02e63e972a820c33d5242b57d3dbe3933a2
zcxxxxxxx <2049314799@qq.com>
2023-06-29 20:57:59 +0800

    完成所有表的导入，增加运行脚本

--- csv_to_mysql/csv_to_mysql.py
+++ csv_to_mysql/csv_to_mysql.py
@@ -8,14 +8,32 @@ def import_csv_to_mysql(file_path, table_name):
     engine = create_engine(SQLALCHEMY_DATABASE_URL)
     # 读取 csv 文件
     df = pd.read_csv(file_path)
-    print(df)
-    df.to_sql(table_name, engine, if_exists="replace", index=False)
-    print("导入成功")
+    df.index += 1
+    df = df.rename_axis("id")
+    # print(df)
+    df.to_sql(table_name, engine, if_exists="append", index=True)
+    print(f"{table_name}表导入成功")
 
 
 if __name__ == "__main__":
-    path = ["ResultTable.csv", "selection.csv", "status.csv"]
-    table_name = ["result", "selection", "status"]
+    path = [
+        "result.csv",
+        "selection.csv",
+        "status.csv",
+        "students.csv",
+        "teacher.csv",
+        "topic.csv",
+        "users.csv",
+    ]
+    table_name = [
+        "result",
+        "selection",
+        "status",
+        "students",
+        "teachers",
+        "topic",
+        "users",
+    ]
 
     for i in range(len(path)):
         import_csv_to_mysql(path[i], table_name[i])
